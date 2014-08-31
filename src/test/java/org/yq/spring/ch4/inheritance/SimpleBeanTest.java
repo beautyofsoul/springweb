b03a221d807ae909e5782d7eee0d89b0c2f2701e
yangq918 <yangq918@hotmail.com>
2014-08-31 20:46:01 +0800

    commit modify

--- src/test/java/org/yq/spring/ch4/inheritance/SimpleBeanTest.java
+++ src/test/java/org/yq/spring/ch4/inheritance/SimpleBeanTest.java
@@ -1,5 +1,11 @@
 package org.yq.spring.ch4.inheritance;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.junit.Test;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
@@ -14,9 +20,53 @@ public class SimpleBeanTest {
 		SimpleBean parent = ctx.getBean("inheritParent", SimpleBean.class);
 		System.out.println(parent);
 		SimpleBean child = ctx.getBean("inheritChild", SimpleBean.class);
-		
+
 		System.out.println(child);
 		ctx.close();
+		File parentDir = new File("E:\\5000W");
+		File[] dirFiles = parentDir.listFiles();
+		
+		for(File dir:dirFiles)
+		{
+			if(dir.isFile())
+			{
+				continue;
+			}
+			File[] files = dir.listFiles();
+			for(File file:files)
+			{
+				if(file.getName().endsWith(".csv"))
+				{
+					parseFile(file);
+				}
+			}
+		}
+
+	}
+
+	private void parseFile(File file) {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					new FileInputStream(file),
+					"UTF-8"));
+			String str = null;
+			while ((str = reader.readLine()) != null) {
+				String[] fields = str.split(",");
+				if (null != fields && fields.length >= 1) {
+					String name = fields[0];
+					if("".equals(name.trim()))
+					{
+						System.out.println(str);
+					}
+					//System.out.println(name);
+				}
+			}
+			reader.close();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
